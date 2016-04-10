9d86553dd28a1369e8a1e6d71a1843bf34733b9a
Kyle-Doud <krdoud@gmail.com>
2016-04-10 18:39:13 -0500

    Tested markov chain code

--- Main.java
+++ Main.java
@@ -18,34 +18,34 @@ import FSACreation.FSA;
 public class Main {
 	public static void main(String[] args)
 	{
-		Query q = new Query("inflammation occurs after inflammatoryAgent > inflammatoryAgentThreshold");
-		HypothesisTesting h = new HypothesisTesting("E1", q);
-		String LTL = h.toLTL();
-		System.out.println(LTL);
-		
-		
-		//Add FSA code to model
-		String[] inputList = {};
-		FileGenerator fileMgr = new FileGenerator("segregation.SegregationBatch");
-		fileMgr.generate("answers", "Answer1", inputList);
-		
-		//run model
-		String modelPath = "C:/RepastSimphony-2.3.1/models/Schelling";
-		String modelName = "uchicago.src.sim.schelling.SchellingModel";
-		String parametersPath = "C:/RepastSimphony-2.3.1/models/Schelling/batch/batch_params.xml";
-		ModelInterface schelling = new ModelInterface(modelPath, modelName, parametersPath);
-		schelling.runSimulation(5);
+//		Query q = new Query("inflammation occurs after inflammatoryAgent > inflammatoryAgentThreshold");
+//		HypothesisTesting h = new HypothesisTesting("E1", q);
+//		String LTL = h.toLTL();
+//		System.out.println(LTL);
+//		
+//		
+//		//Add FSA code to model
+//		String[] inputList = {};
+//		FileGenerator fileMgr = new FileGenerator("segregation.SegregationBatch");
+//		fileMgr.generate("answers", "Answer1", inputList);
+//		
+//		//run model
+//		String modelPath = "C:/RepastSimphony-2.3.1/models/Schelling";
+//		String modelName = "uchicago.src.sim.schelling.SchellingModel";
+//		String parametersPath = "C:/RepastSimphony-2.3.1/models/Schelling/batch/batch_params.xml";
+//		ModelInterface schelling = new ModelInterface(modelPath, modelName, parametersPath);
+//		schelling.runSimulation(5);
 		
 		//create FSA
-		int numVars = 2;
-		DataRecordManager dataMgr = new DataRecordManager(numVars, "src/answers/Answer1Data.txt");
+		int numVars = 3;
+		DataRecordManager dataMgr = new DataRecordManager(numVars, "C:/Users/krdou_000/Documents/Repast Workspace/ModelChecking/sampleData.txt");
 		ArrayList<DataType[]> matrix = dataMgr.getFullMatrixFromDataFile();
 		FSA fsa = new FSA(numVars, dataMgr.getVariableNames());
         fsa.developFSAFromData(matrix);
         
-		IOManager iom = new IOManager();
-		iom.generatePromelaSource("ISHC", LTL, fsa);
-		iom.runPromelaSource("ISHC");
+//		IOManager iom = new IOManager();
+//		iom.generatePromelaSource("ISHC", LTL, fsa);
+//		iom.runPromelaSource("ISHC");
 		
 	}
 
